f3156ee0397ffe1af8524f30b1bc63285e2c3dcc
EugeFranchi <eugefranx@gmail.com>
2017-06-30 19:15:39 -0300

    ya esta en base de datos

--- deuda_parcial.py
+++
@@ -1,8 +0,0 @@
-def pagar_deuda_parcial(self,nombre,cantidad):
-    """Resibe el nombre del cliente y la cantidad pagada por el cliente. modifica el archivo 'resumen.txt' con la nueva cantidad que debe"""
-    if not existe(nombre):
-        raise ValueError("el nombre no existe")
-    nuevos_montos = self.resumen_a_lista(nombre,-cantidad)
-    self.lista_a_resumen(nuevos_montos)
-    nombre_archivo = self.nombre_archivo_cliente(nombre)
-    self.deuda_a_cliente(nombre_archivo,-cantidad)
