bd3b9f1943d6ca507505e3a5d88e305b274ec0b9
EugeFranchi <eugefranx@gmail.com>
2017-07-02 16:34:39 -0300

    Update ticket.py

--- ticket.py
+++ ticket.py
@@ -13,3 +13,19 @@ class Ticket:
         self.archivo.create()
         self.nombre = self.archivo.nombre
         self.database = BD(database)
+    
+    
+    def add(self, cliente, col, pagado, vendedor):
+        """
+        Agrega una linea de texto al ticket.
+        """
+        
+        datos = self.database.consulta(cliente)
+        deuda = datos[int(col)]
+        data = ",".join([cliente.lower(), str(deuda), str(pagado), time.strftime("%d/%m/%y"), vendedor])
+        
+        with open(self.nombre, "r+") as file:
+            linea = file.readline()
+            while linea:
+                linea = file.readline()
+            file.write(data + "\n")
