3a4142375421d4a14d7464f794abe28de4ab4e0d
EugeFranchi <eugefranx@gmail.com>
2017-07-02 16:24:03 -0300

    Update ticket.py

--- ticket.py
+++ ticket.py
@@ -1,12 +1,15 @@
 from archivo import ARCHIVO
+from BD import BD
 import time
+import csv
 
 class Ticket:
     
-    def __init__(self, nombre):
+    def __init__(self, nombre, database):
         """
         Inicializa el Ticket.
         """
         self.archivo = ARCHIVO(nombre)
         self.archivo.create()
         self.nombre = self.archivo.nombre
+        self.database = BD(database)
