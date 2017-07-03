2c6f815fe3b3aa8091ea68e15e76c6f14cf272c3
masacr3 <leorhcp2030@gmail.com>
2017-07-02 22:10:36 -0300

    add ADD_TIKET

--- add_resumen.py
+++ add_resumen.py
@@ -1,6 +1,7 @@
 #Code by @masacr3
 from * import nodo
 from * import DB
+from * import TIKET
 
 def add_resumen(cliente):
 	db = DB("resumen")
@@ -8,12 +9,28 @@ def add_resumen(cliente):
 		return
 	db.add(cliente.name,cliente.total)
 
-def update_resumen(cliente):
+def update_resumen(cliente,totalNuevo):
 	db = DB("resumen")
 	if not db.esta_id(cliente.nombre):
 		return
-	totalNuevo= int(db.consulta(cliente.nombre)[1]) + cliente.total
 	db.update(nombre.cliente,[1],[totalNuevo])
+
+
+def add_tiket(cliente,pago,vendedor):
+	tiketDB = TIKET("tiket")
+	resumenDB = DB("resumen")
+
+	if not resumenDB.esta_id(cliente.nombre):
+		return
+
+	ID,deuda = resumenDB.consulta(cliente.nombre)
+	tiketDB.add(ID,deuda,pago,vendedor)
+
+	saldo = deuda - pago
+	if (saldo)<=0:
+		resumenDB.delete(ID)
+
+	update_resumen(cliente,saldo)
 
 
 
