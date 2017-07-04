955405212f0e89740523100e476387babf5b798b
masacr3 <leorhcp2030@gmail.com>
2017-07-04 14:57:55 -0300

    idea?

---
+++ grid.py
@@ -0,0 +1,17 @@
+class GRID:
+	def __init__(self):
+		self.items = []
+		self.len = 0 
+
+	def push(self,*items):
+		for element in item:
+			lista.append(element)
+		self.items.append(",".join(lista))
+		self.len +=1
+
+	def show(self):
+		i = 0 
+		for item in items:
+			print(item)
+
+# la idea seria pasar esto con listas enlazadas simples
