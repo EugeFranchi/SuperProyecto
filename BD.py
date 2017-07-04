0df6320c91e7f7f40772f0143da823c0959df85a
EugeFranchi <eugefranx@gmail.com>
2017-07-03 23:28:20 -0300

    saque el * de *dato de add

--- BD.py
+++ BD.py
@@ -36,7 +36,7 @@ class BD:
         return self.consulta(ID) != []
     
     
-    def add(self, *datos):
+    def add(self, datos):
         """
         Recibe una lista de datos y la agrega a la
         base de datos.
